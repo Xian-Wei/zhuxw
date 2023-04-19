dbe0006bd680065d81586f65ffb5585ce263c8bd
Xian-Wei <xianweizhu@hotmail.com>
2023-04-19 22:55:42 +0200

    Added NFT shop to Web3 page
    - Added NFT shop in the sidebar
    - Approve + Mint function

---
+++ pages/shop/NFTCard/index.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import styles from "./nftcard.module.scss";
+import Image from "next/image";
+import LoadingAnimation from "../../../components/LoadingAnimation";
+
+interface NFTCardInterface {
+  title: string;
+  price: number;
+  image: string;
+  alt: string;
+  approve: () => Promise<boolean | undefined>;
+  mint: () => Promise<boolean | undefined>;
+  isInteractible: boolean;
+}
+
+const NFTCard = ({
+  title,
+  price,
+  image,
+  alt,
+  approve,
+  mint,
+  isInteractible,
+}: NFTCardInterface) => {
+  const [approved, setApproved] = useState(false);
+  const [minted, setMinted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onApprove = async () => {
+    setIsLoading(true);
+    const success = await approve();
+    if (success) {
+      setApproved(true);
+    }
+    setIsLoading(false);
+  };
+
+  const onMint = async () => {
+    setIsLoading(true);
+    const success = await mint();
+    if (success) {
+      setMinted(true);
+    }
+    setIsLoading(false);
+  };
+
+  const MintButton = () => {
+    if (isInteractible) {
+      if (isLoading) {
+        return (
+          <div className={styles.disabledButton}>
+            <LoadingAnimation />
+          </div>
+        );
+      } else if (approved) {
+        if (minted) {
+          return <div className={styles.mintButton}>Minted !</div>;
+        } else {
+          return (
+            <div className={styles.mintButton} onClick={onMint}>
+              Mint
+            </div>
+          );
+        }
+      } else {
+        return (
+          <div className={styles.mintButton} onClick={onApprove}>
+            Approve
+          </div>
+        );
+      }
+    } else {
+      return <div className={styles.disabledButton}>Not available</div>;
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Image
+        width={300}
+        height={300}
+        alt={alt}
+        src={image}
+        className={styles.image}
+      />
+      <div className={styles.info}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.price}>{price} $ZHU</div>
+        <MintButton />
+      </div>
+    </div>
+  );
+};
+
+export default NFTCard;
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,11 +1,133 @@
 import Head from "next/head";
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import MetaTags from "../../components/MetaTags";
 import styles from "./shop.module.scss";
+import useWeb3ChainId from "../../hooks/useWeb3Network";
+import contractAddresses from "../../data/artifacts/contractAddresses.json";
+import zhuAbi from "../../data/artifacts/Zhu.json";
+import zhubaAbi from "../../data/artifacts/Zhuba.json";
+import { ethers } from "ethers";
+import useWeb3Provider from "../../hooks/useWeb3Provider";
+import NFTCard from "./NFTCard";
 
 export const siteTitle = "XWZ Web3 Shop";
 
 const Shop = () => {
+  const provider: ethers.providers.Web3Provider | null = useWeb3Provider();
+  const chainId: number | null = useWeb3ChainId();
+  const zhuContractAddress: string | null = chainId
+    ? String(chainId) in contractAddresses
+      ? contractAddresses[String(chainId) as keyof typeof contractAddresses][
+          "Zhu"
+        ][0]
+      : null
+    : null;
+  const zhubaContractAddress: string | null = chainId
+    ? String(chainId) in contractAddresses
+      ? contractAddresses[String(chainId) as keyof typeof contractAddresses][
+          "Zhuba"
+        ][0]
+      : null
+    : null;
+
+  const [balance, setBalance] = useState("0");
+  const [isInteractible, setIsInteractible] = useState(false);
+
+  const approve = async () => {
+    if (zhuContractAddress && zhubaContractAddress && provider) {
+      try {
+        const signer = await provider.getSigner();
+        const zhuContract = new ethers.Contract(
+          zhuContractAddress,
+          zhuAbi,
+          signer
+        );
+        const zhubaContract = new ethers.Contract(
+          zhubaContractAddress,
+          zhubaAbi,
+          signer
+        );
+        await provider.send("eth_requestAccounts", []);
+
+        const mintFee = await zhubaContract.getMintFee();
+
+        const tx = await zhuContract._approve(zhubaContractAddress, mintFee, {
+          gasLimit: 100000,
+        });
+        await tx.wait();
+        return true;
+      } catch (e) {
+        console.error(e);
+        return false;
+      }
+    }
+  };
+
+  const mint = async () => {
+    if (zhubaContractAddress && provider) {
+      try {
+        const signer = await provider.getSigner();
+        const zhubaContract = new ethers.Contract(
+          zhubaContractAddress,
+          zhubaAbi,
+          signer
+        );
+        await provider.send("eth_requestAccounts", []);
+
+        const tx = await zhubaContract.requestNft({
+          gasLimit: 3000000,
+        });
+        await tx.wait();
+        await getBalance();
+        return true;
+      } catch (e) {
+        console.error(e);
+        return false;
+      }
+    }
+  };
+
+  const getBalance = useCallback(async () => {
+    if (zhuContractAddress && provider) {
+      try {
+        const zhuContract = new ethers.Contract(
+          zhuContractAddress,
+          zhuAbi,
+          provider
+        );
+        let accounts = await provider.send("eth_requestAccounts", []);
+        let account = accounts[0];
+
+        const balance = await zhuContract.balanceOf(account);
+
+        if (balance.toString() != "0") {
+          setBalance(
+            balance.toString().substring(0, balance.toString().length - 18)
+          );
+        } else setBalance("0");
+
+        return true;
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      setBalance("0");
+      return false;
+    }
+  }, [chainId]);
+
+  useEffect(() => {
+    (async () => {
+      await getBalance();
+    })();
+  }, [chainId]);
+
+  useEffect(() => {
+    if (provider && zhuContractAddress && zhubaContractAddress) {
+      setIsInteractible(true);
+    } else setIsInteractible(false);
+  }, [provider, zhuContractAddress, zhubaContractAddress]);
+
   return (
     <>
       <Head>
@@ -19,7 +141,18 @@ const Shop = () => {
         />
       </Head>
       <div className={styles.container}>
-        <div className={styles.shopButton}>Shop</div>
+        <div className={styles.balance}>Balance: {balance} $ZHU</div>
+        <div className={styles.card}>
+          <NFTCard
+            title="Zhuba NFT Gacha"
+            price={10000}
+            image="/../public/images/zhuba.gif"
+            alt="Zhuba GIF"
+            approve={approve}
+            mint={mint}
+            isInteractible={isInteractible}
+          />
+        </div>
       </div>
     </>
   );
