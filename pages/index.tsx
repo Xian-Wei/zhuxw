8d609c05170a0bf7f619b49336c7b13d4dc8451d
Xian-Wei <xianweizhu@hotmail.com>
2023-06-11 23:49:21 +0200

    Added particles to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Head from "next/head";
 import { Canvas } from "@react-three/fiber";
 import Layout from "../components/Layout";
@@ -7,11 +7,21 @@ import styles from "../styles/Home.module.scss";
 import { Experience } from "../components/3D/Experience";
 import useIsWidth from "../hooks/useIsWidth";
 import { WindowWidth } from "../models/WindowWidth";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
 
 export const siteTitle = "Xian-Wei Zhu";
 
 export default function Home() {
   const isWidth = useIsWidth(WindowWidth.md);
+
+  const particlesInit = async (main: Engine) => {
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
 
   return (
     <Layout
@@ -30,6 +40,14 @@ export default function Home() {
         />
       </Head>
       <main className={styles.container}>
+        <div className={styles.particles}>
+          <Particles
+            className={styles.particles}
+            init={particlesInit}
+            options={particlesOptions as any}
+          />
+        </div>
+
         <div className={styles.heroContent}>
           <div className={styles.text}>Xian-Wei Zhu</div>
           <Canvas
@@ -43,3 +61,101 @@ export default function Home() {
     </Layout>
   );
 }
+
+const particlesOptions = {
+  fullScreen: {
+    enable: true,
+  },
+  particles: {
+    number: {
+      value: 400,
+      density: {
+        enable: true,
+      },
+    },
+    color: {
+      value: Math.floor(Math.random() * 16777215).toString(16),
+    },
+    shape: {
+      type: "circle",
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 500,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "bottom",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 0.5,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 4,
+        duration: 0.3,
+        opacity: 1,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+};
