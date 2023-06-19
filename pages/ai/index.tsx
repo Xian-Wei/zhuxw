18cab8e9d6dab87aa7083a159ddfe2b6f6302981
Xian-Wei <xianweizhu@hotmail.com>
2023-06-19 22:07:19 +0200

    Added auto-scroll and styling to AI Chat

--- pages/ai/index.tsx
+++ pages/ai/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Layout from "../../components/Layout";
 import styles from "./ai.module.scss";
 import { useChat } from "ai/react";
@@ -7,21 +7,35 @@ const AI = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: "/api/chat",
   });
+  const chatList = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    chatList.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <Layout navbarEnabled={true} footerEnabled={false} backgroundEnabled={true}>
       <div className={styles.container}>
         <div className={styles.chat}>
-          {messages.map((m) => (
-            <div key={m.id} className={styles.conversation}>
-              {m.role}: {m.content}
-            </div>
-          ))}
+          <div className={styles.chatLines}>
+            {messages.map((m) => (
+              <div
+                key={m.id}
+                className={
+                  m.role == "user" ? styles.chatLine : styles.chatLineAi
+                }
+              >
+                {m.content}
+              </div>
+            ))}
+            <div ref={chatList} />
+          </div>
 
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className={styles.chatForm}>
             <input
               value={input}
               placeholder="Say something..."
+              className={styles.chatInput}
               onChange={handleInputChange}
             />
           </form>
