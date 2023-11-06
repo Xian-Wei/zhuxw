1170c5729a1dc7eef7721f4bf5525e424983aafe
Xian-Wei <xianweizhu@hotmail.com>
2023-11-06 23:50:05 +0100

    CORS fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
 const nextConfig = {
   reactStrictMode: false,
   images: { unoptimized: false },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.zhuxw.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
