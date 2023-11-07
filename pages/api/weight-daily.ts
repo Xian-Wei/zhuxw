f8fc1962058b99234b02523a20fe6566fbf39def
Xian-Wei <xianweizhu@hotmail.com>
2023-11-07 05:13:11 +0100

    Changed Firestore doc name

--- pages/api/weight-daily.ts
+++ pages/api/weight-daily.ts
@@ -11,7 +11,7 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   try {
-    const docRef = doc(db, "weight", "xw");
+    const docRef = doc(db, "weights", "xw");
     const docSnap = await getDoc(docRef);
     const dataArray = docSnap.data();
 
--- pages/api/weight-weekly.ts
+++ pages/api/weight-weekly.ts
@@ -33,7 +33,7 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   try {
-    const docRef = doc(db, "weight", "xw");
+    const docRef = doc(db, "weights", "xw");
     const docSnap = await getDoc(docRef);
     const dataArray = docSnap.data();
 
