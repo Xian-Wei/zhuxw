b2bbdddcc12985b0fc0a08eb93e6594c598c28b1
Xian-Wei <xianweizhu@hotmail.com>
2023-12-05 19:10:48 +0100

    Added API key to weight API

--- pages/api/weight.ts
+++ pages/api/weight.ts
@@ -18,63 +18,67 @@ export default async function handler(
     return;
   }
 
-  if (req.body.date && req.body.weight && dateRegex.test(req.body.date)) {
-    try {
-      const docRef = doc(db, "weights", "xw");
-      const docSnap = await getDoc(docRef);
-      const weights = docSnap.data()?.weights as Weight[];
-      let weightsIndex = -1;
-      let newWeight: Weight = {
-        time: req.body.date,
-        open: -1,
-        high: -1,
-        low: -1,
-        close: Number(req.body.weight),
-      };
+  if (req.headers.key == process.env.NEXT_PUBLIC_ZHUXW_API_KEY) {
+    if (req.body.date && req.body.weight && dateRegex.test(req.body.date)) {
+      try {
+        const docRef = doc(db, "weights", "xw");
+        const docSnap = await getDoc(docRef);
+        const weights = docSnap.data()?.weights as Weight[];
+        let weightsIndex = -1;
+        let newWeight: Weight = {
+          time: req.body.date,
+          open: -1,
+          high: -1,
+          low: -1,
+          close: Number(req.body.weight),
+        };
 
-      weights.map((weight: Weight, index) => {
-        if (weight.time == req.body.date) {
-          weightsIndex = index;
+        weights.map((weight: Weight, index) => {
+          if (weight.time == req.body.date) {
+            weightsIndex = index;
+          }
+        });
+
+        if (weightsIndex === -1) {
+          newWeight.open = Number(weights[weights.length - 1].close);
+          newWeight.high =
+            req.body.weight >= weights[weights.length - 1].close
+              ? Number(req.body.weight)
+              : Number(weights[weights.length - 1].close);
+          newWeight.low =
+            req.body.weight >= weights[weights.length - 1].close
+              ? Number(weights[weights.length - 1].close)
+              : Number(req.body.weight);
+
+          weights.push(newWeight);
+
+          await setDoc(doc(db, "weights", "xw"), { weights: weights });
+
+          res.status(200).send({ message: "Added new weight" });
+        } else {
+          newWeight.open = Number(weights[weights.length - 2].close);
+          newWeight.high =
+            req.body.weight >= weights[weights.length - 2].close
+              ? Number(req.body.weight)
+              : Number(weights[weights.length - 2].close);
+          newWeight.low =
+            req.body.weight >= weights[weights.length - 2].close
+              ? Number(weights[weights.length - 2].close)
+              : Number(req.body.weight);
+
+          weights[weightsIndex] = newWeight;
+
+          await setDoc(doc(db, "weights", "xw"), { weights: weights });
+
+          res.status(200).send({ message: "Updated weight" });
         }
-      });
-
-      if (weightsIndex === -1) {
-        newWeight.open = Number(weights[weights.length - 1].close);
-        newWeight.high =
-          req.body.weight >= weights[weights.length - 1].close
-            ? Number(req.body.weight)
-            : Number(weights[weights.length - 1].close);
-        newWeight.low =
-          req.body.weight >= weights[weights.length - 1].close
-            ? Number(weights[weights.length - 1].close)
-            : Number(req.body.weight);
-
-        weights.push(newWeight);
-
-        await setDoc(doc(db, "weights", "xw"), { weights: weights });
-
-        res.status(200).send({ message: "Added new weight" });
-      } else {
-        newWeight.open = Number(weights[weights.length - 2].close);
-        newWeight.high =
-          req.body.weight >= weights[weights.length - 2].close
-            ? Number(req.body.weight)
-            : Number(weights[weights.length - 2].close);
-        newWeight.low =
-          req.body.weight >= weights[weights.length - 2].close
-            ? Number(weights[weights.length - 2].close)
-            : Number(req.body.weight);
-
-        weights[weightsIndex] = newWeight;
-
-        await setDoc(doc(db, "weights", "xw"), { weights: weights });
-
-        res.status(200).send({ message: "Updated weight" });
+      } catch (e: any) {
+        res.status(404).json({ message: e });
       }
-    } catch (e: any) {
-      res.status(404).json({ message: e });
+    } else {
+      res.status(400).send({ message: "Wrong format" });
     }
   } else {
-    res.status(400).send({ message: "Wrong format" });
+    res.status(401).send({ message: "Invalid API key" });
   }
 }
