5da7891bf3c861d550e16b5d12b855cbfb78bea1
Xian-Wei <xianweizhu@hotmail.com>
2024-01-04 23:29:31 +0100

    Disable Vercel Analytics debug logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
-      <Analytics />
+      <Analytics debug={false} />
     </Provider>
   );
 }
