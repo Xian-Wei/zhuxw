d587a53d435bc9deb3ac56cb6545ee1aeecb71e4
Xian-Wei <xianweizhu@hotmail.com>
2024-01-12 01:49:54 +0100

    Adjusted Web3Wallet and Navbar to fit the centered logo

--- components/Navbar/Web3Wallet/index.tsx
+++ components/Navbar/Web3Wallet/index.tsx
@@ -2,11 +2,16 @@ import styles from "./web3wallet.module.scss";
 import useWeb3Provider from "../../../hooks/useWeb3Provider";
 import useIsMetamaskInstalled from "../../../hooks/useIsMetamaskInstalled";
 import useWeb3Wallet from "../../../hooks/useWeb3Wallet";
+import useIsWidth from "../../../hooks/useIsWidth";
+import { WindowWidth } from "../../../models/WindowWidth";
 
 const Web3Wallet = () => {
   const { wallet, setWallet } = useWeb3Wallet();
   const provider = useWeb3Provider();
   const isMetamaskInstalled = useIsMetamaskInstalled();
+  const isWidth = useIsWidth(WindowWidth.md);
+  const shortenedWallet =
+    wallet !== "" ? `${wallet.substring(0, 5)}...${wallet.slice(-3)}` : "";
 
   const connect = async () => {
     try {
@@ -23,17 +28,19 @@ const Web3Wallet = () => {
     }
   };
 
-  if (isMetamaskInstalled) {
-    return (
-      <div className={styles.button} onClick={connect}>
-        {wallet != ""
-          ? wallet.substring(0, 5) +
-            "..." +
-            wallet.substring(wallet.length - 3, wallet.length)
-          : "Connect"}
-      </div>
-    );
-  } else return <> </>;
+  if (!isMetamaskInstalled) {
+    return null;
+  }
+
+  if (!isWidth && wallet !== "") {
+    return null;
+  }
+
+  return (
+    <div className={styles.button} onClick={connect}>
+      {wallet !== "" ? shortenedWallet : "Connect"}
+    </div>
+  );
 };
 
 export default Web3Wallet;
