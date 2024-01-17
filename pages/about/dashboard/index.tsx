5d388c14001f8728dcca2a78c0a2249e42247973
Xian-Wei <xianweizhu@hotmail.com>
2024-01-17 08:53:14 +0100

    Styled Dashboard grid

--- pages/about/dashboard/index.tsx
+++ pages/about/dashboard/index.tsx
@@ -3,12 +3,14 @@ import styles from "./dashboard.module.scss";
 
 const Dashboard = () => {
   return (
-    <div className={styles.content}>
-      <div className={styles.gridContainer}>
-        <div className={styles.gridItem}>A</div>
-        <div className={styles.gridItem}>B</div>
-        <div className={styles.gridItem}>C</div>
-        <div className={styles.gridItem}>D</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.gridContainer}>
+          <div className={styles.gridItem}>A</div>
+          <div className={styles.gridItem}>B</div>
+          <div className={styles.gridItem}>C</div>
+          <div className={styles.gridItem}>D</div>
+        </div>
       </div>
     </div>
   );
