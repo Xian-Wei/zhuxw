3e7e381e26b77e21c7ea90f7955c3fd8d54bccee
Xian-Wei <xianweizhu@hotmail.com>
2024-01-30 01:11:39 +0100

    Added Dashboard top info

--- pages/about/dashboard/index.tsx
+++ pages/about/dashboard/index.tsx
@@ -1,15 +1,81 @@
 import React from "react";
 import styles from "./dashboard.module.scss";
+import axios from "axios";
+import { Weight } from "../../../models/Weight";
+import useSWR from "swr";
+import { WorkoutItem } from "../../../models/WorkoutItem";
+import { formatDateAgo, getCurrentMonth } from "../../../utils/Date";
+import useIsMetamaskInstalled from "../../../hooks/useIsMetamaskInstalled";
 
 const Dashboard = () => {
+  const fetcher = (url: string) => axios.get(url).then(res => res.data);
+  const { data: dailyWeights }: { data: Weight[] } = useSWR(
+    "/api/weight-daily",
+    fetcher,
+  );
+  const { data: workouts }: { data: WorkoutItem[] } = useSWR(
+    "/api/workout",
+    fetcher,
+  );
+  const isMetamaskInstalled = useIsMetamaskInstalled();
+
+  const getWeightTrend = (days: number) => {
+    const weightDifference =
+      dailyWeights[dailyWeights.length - 1].close -
+      dailyWeights[dailyWeights.length - days - 1].close;
+
+    if (weightDifference < 0) {
+      return isMetamaskInstalled ? "Bearish" : "Optimistic";
+    } else if (weightDifference > 0) {
+      return isMetamaskInstalled ? "Bullish" : "Pessimistic";
+    } else {
+      return "Neutral";
+    }
+  };
+
+  const getWorkoutCountForMonth = () => {
+    const currentMonth = getCurrentMonth();
+    let workoutCount = 0;
+
+    if (workouts) {
+      for (let i = workouts.length - 1; i > workouts.length - 30; i--) {
+        if (workouts[i].date.substring(5, 7) == currentMonth) {
+          workoutCount++;
+        }
+      }
+      return workoutCount;
+    } else return 0;
+  };
+
+  const todayWeight = dailyWeights
+    ? dailyWeights[dailyWeights.length - 1].close
+    : 0;
+  const weightTrend = dailyWeights ? getWeightTrend(14) : "Neutral";
+  const lastWorkout =
+    workouts && workouts.length > 0
+      ? formatDateAgo(workouts[workouts.length - 1].date)
+      : "None";
+  const workoutCountsForMonth =
+    workouts && workouts.length > 0 ? getWorkoutCountForMonth() : 0;
+
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.gridContainer}>
-          <div className={styles.gridItem}>A</div>
-          <div className={styles.gridItem}>B</div>
-          <div className={styles.gridItem}>C</div>
-          <div className={styles.gridItem}>D</div>
+    <div className={styles.content}>
+      <div className={styles.topInfos}>
+        <div className={styles.topInfoItem}>
+          Today&apos;s weight
+          <div className={styles.topInfoText}>{todayWeight} KG</div>
+        </div>
+        <div className={styles.topInfoItem}>
+          Weight trend
+          <div className={styles.topInfoText}>{weightTrend}</div>
+        </div>
+        <div className={styles.topInfoItem}>
+          Last workout
+          <div className={styles.topInfoText}>{lastWorkout}</div>
+        </div>
+        <div className={styles.topInfoItem}>
+          Workouts (month)
+          <div className={styles.topInfoText}>{workoutCountsForMonth}</div>
         </div>
       </div>
     </div>
